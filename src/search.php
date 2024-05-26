5e7665ae0d558d55eab9c71f7b67851d1d61905a
JerkoJonckersZP <jerko.jonckers@bazandpoort.be>
2024-05-26 23:35:07 +0200

    Fixed menu

--- src/search.php
+++ src/search.php
@@ -15,6 +15,14 @@
             </a>";
             if(isset($_SESSION['userid'])) {
                 echo '
+                <a href="liked-posts.php">
+                    <button class="btn btn-ghost justify-start w-full mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+                        LIKED POSTS
+                    </button>
+                </a>
                 <button class="btn w-full" onclick="create_post_modal.showModal()">CREATE POST</button>
                 ';
             }
